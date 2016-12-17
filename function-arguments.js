0150b012ebe0a701e09cb03ef613abd81fb04762
John <johnkmeas@gmail.com>
2016-12-17 05:18:25 +0000

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(num, numb, number){
+  return num + numb * number;
+}
+
+console.log(math(53, 61, 67));
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+var pizza = 'pizza is alright';
+var wonderful = pizza.replace('alright', 'wonderful');
+console.log(wonderful);
